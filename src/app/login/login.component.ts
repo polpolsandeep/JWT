8acc6476096b86db91a78cf4fe491f561987e960
sandeep kumar <sndpkumar322@gmail.com>
2024-04-03 14:19:01 +0530

    this is jwt

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { UserService } from '../service/user.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  constructor(private userService:UserService){}
+
+  userLogin(data:any){
+     console.warn(data);
+     this.userService.login(data)
+  }
+
+}
